6cf17d36ec226d81711aeda76ce4d9cce88af33d
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2016-07-30 17:14:23 +1200

    Now geenrating threads based on threads.json

--- config.rb
+++ config.rb
@@ -40,6 +40,15 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
+data.threads.each do |thread|
+	proxy "/threads/#{thread[:id]}/index.html",
+				"templates/generator_thread.html",
+				:locals => {
+					pairs: thread[:pairs]
+				},
+				:ignore => true
+end
+
 # General configuration
 
 activate :directory_indexes

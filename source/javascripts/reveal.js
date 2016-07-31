6a9259fb081c0b2ed1bb0439c2119988300893db
David Barnett <david@barnett.net.nz>
2016-07-31 12:29:10 +1200

    scroll to the answer and allow for changes

--- source/javascripts/reveal.js
+++ source/javascripts/reveal.js
@@ -3,22 +3,35 @@ $('.panel-question').each(function(_, question) {
   // keep the question id
   var questionId = question.id;
   var revealed = false;
+  var answerDivs = [];
 
   // find the options for the questions
   $(question).find('input').each(function(_, answer) {
 
     // generate the id to the answer div
     var divSelector =  '#' + answer.id.replace(questionId, 'answer');
+    // keep a list of answer's ids
+    answerDivs.push(divSelector);
 
     // make the whole list item being clickable
     var btn = $(answer).parent();
     btn.click(function() {
+      // show/hide all the answers
+      $.each(answerDivs, function(_, selector) {
+        var answer = $(selector);
 
-      // only reveal one
-      if (!revealed) {
-        $(divSelector).show();
-        revealed = true;
-      }
+        if (selector === divSelector) {
+          answer.show();
+
+          // smooth scrolling to the answer
+          $('html, body').stop().animate({
+            scrollTop: answer.offset().top
+          }, 500);
+
+        } else {
+          answer.hide();
+        }
+      });
     });
 
   });

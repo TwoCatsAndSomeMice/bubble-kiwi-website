d8c4fc49d6f84f43e76c269aeaa4bbc6821079fb
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2016-07-31 14:37:44 +1200

    Bugfixes

--- config.rb
+++ config.rb
@@ -37,7 +37,7 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-data.threads.each do |thread|
+data.thread.each do |thread|
 	proxy "/threads/#{thread[:id]}/index.html",
 				"templates/generator_thread.html",
 				:locals => {
